52f2eb29132307d6018d5c23bc63520234d939dd
HaoHamHoc <145907450+HaoHamHoc@users.noreply.github.com>
2025-03-09 09:46:40 +0700

    update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
-@vite(['resources/css/auth/login.css', 'resources/css/custom.css'])
+@vite(['resources/css/auth/login.css','resources/css/app.css'])
 @section('title', 'Đăng nhập')
 @include('layouts.partials.meta')
-
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -15,27 +14,27 @@
             <!-- Cột phải -->
             <div class="col-md-6 d-flex align-items-center justify-content-center">
                 <div class="w-75">
-                    <h2>Get started.</h2>
-                    <p>Already have an account? <a href="#">Sign up</a></p>
+                    <h1>Get started.</h1>
+                    <p style="font-size: 1.2rem;">Already have an account? <a href="#">Sign up</a></p>
 
                     <div class="d-flex gap-3 mt-3">
-                        <button class="btn text-white border-0 w-100" style="background-color: #db4437;">
-                            <img src="{{ asset('img/login/google-icon.png') }}" alt="Google"> Log in with Google
+                        <button class="btn text-black border w-100" style="background-color: #ffffff; display: flex; align-items: center; justify-content: center;">
+                            <img src="{{ asset('img/login/google-icon.png') }}" width="24px" alt="Google"> <span>Log in with Google</span>
                         </button>
-                        <button class="btn text-white border-0 w-100" style="background-color: #3b5998;">
-                            <img src="{{ asset('img/login/facebook-icon.svg') }}" width="16px" alt="Facebook"> Log in
-                            with Facebook
+                        <button class="btn text-white border-0 w-100" style="background-color: #3b5998; display: flex; align-items: center; justify-content: center;">
+                            <img src="{{ asset('img/login/facebook-icon.svg') }}" width="24px" alt="Facebook"> <span>Log in with Facebook</span> 
                         </button>
                     </div>
 
-                    <p class="text-center my-3">or</p>
+
+                    <p class="socials-divider"><span class="text-center my-3" style="font-size: 1.2rem;">or</span></p>
 
                     <form method="POST" action="">
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email address</label>
                             <div class="input-group">
-                                <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                                <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
                                 <input type="email" class="form-control border-0 shadow-none" name="email"
                                     placeholder="example@gmail.com" required>
                             </div>
@@ -44,7 +43,7 @@
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <div class="input-group">
-                                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                                <span class="input-group-text"><i class="fa-solid fa-key"></i></span>
                                 <input type="password" class="form-control border-0 shadow-none" name="password"
                                     placeholder="Your password" required>
                             </div>
