c744905241767a54294034822536080cd2af2fd2
Vo Thanh Tin <tin369cv@gmail.com>
2025-04-02 15:46:06 +0700

    update 02-04-2025
    Update trang quản lý

--- resources/views/admin/quanlysanpham.blade.php
+++ resources/views/admin/quanlysanpham.blade.php
@@ -3,12 +3,11 @@
 @section('content')
 @section('title', 'Quản lý sản phẩm')
     <div class="container">
-        <h2 class="my-4 text-center">Danh sách sản phẩm</h2>
         <div class="table-responsive">
             <table class="table table-striped table-bordered">
                 <thead class="table-dark">
                     <tr>
-                        <th>ID</th>
+                        <th>Thông tin</th>
                         <th>Tên sản phẩm</th>
                         <th>Tóm tắt sản phẩm</th>
                         <th>Ngày tạo</th>
@@ -26,9 +25,10 @@
                             <td>{{$i->updated_at}}</td>
                             <td>
                                 {{-- <button class="btn btn-warning btn-sm me-1">Sửa</button> --}}
-                                <button class="btn btn-danger btn-sm">Xóa</button>
-                                <button class="btn btn-info btn-sm"><a href="quanlysanpham/{{$i->id_sp}}">Sửa
-                                        sản phẩm</a></button>
+                                <button class="btn btn-danger btn-sm"><a class="delete_btn"><i
+                                            class="bi bi-trash3-fill"></i></a></button>
+                                <button class="btn btn-info btn-sm"><a class="edit_btn" href="quanlysanpham/{{$i->id_sp}}"><i
+                                            class="bi bi-pencil-square"></i> Sửa</a></button>
                             </td>
                         </tr>
                     @endforeach
