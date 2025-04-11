7dcb167b18f62384900defb5c5a6d23c072d7d6e
Vo Thanh Tin <tin369cv@gmail.com>
2025-04-11 22:07:09 +0700

    fix bug 11-4-2025

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,6 @@ Route::get('/blog', [BlogController::class, 'blog'])->name('blog');
 Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
 Route::get('/cart', [CartController::class, 'cart']);
 
-
 Route::get('/login', [AuthController::class, 'loginPage']);
 Route::get('/register', [AuthController::class, 'registerPage']);
 Route::get('/verify', [AuthController::class, 'verifyPage']);
